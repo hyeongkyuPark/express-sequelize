ede93c94b3457af756106eaacb25a149a63dae5e
parkhyeongkyu <phg0644@gmail.com>
2020-06-26 20:16:48 +0900

    asynk await 리팩토링

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -2,67 +2,47 @@ const models = require('../../models');
 const { render } = require('../../app');
 const { response, request } = require('express');
 
-exports.get_products = ((request, response)=> {
+exports.get_products = (async (request, response)=> {
     // response.render('admin/products.html')
-    models.Products.findAll({
-        
-    })
-    .then((productList) => {
-        response.render('admin/products.html',{
-            productList
-        });
-    });
+    const productList = await models.Products.findAll();
+    response.render('admin/products.html',{productList});
 });
-
 
 exports.get_products_write = ((request, response)=> {
     response.render('admin/write.html')
 });
-exports.post_products_write = ((request, response)=> {
+exports.post_products_write = (async (request, response)=> {
     // response.send(request.body);
-    models.Products.create(request.body)
-    .then(()=>{
-        response.redirect('/admin/products');
-    });
+    await models.Products.create(request.body)
+    response.redirect('/admin/products');
 });
 
 
-exports.get_products_detail = ((request, response)=> {
-    models.Products.findByPk(request.params.id)
-    .then((product)=> {
-        response.render('admin/detail.html', {
-            product
-        })
-    });
+exports.get_products_detail = (async (request, response)=> {
+    const product = await models.Products.findByPk(request.params.id);
+    response.render('admin/detail.html', {product});
 });
 
 
-exports.get_products_edit = ((request, response)=> {
-    models.Products.findByPk(request.params.id)
-    .then((product)=> {
-        response.render('admin/edit.html', {
-            product
-        })
-    });
+exports.get_products_edit = (async (request, response)=> {
+    const product = await models.Products.findByPk(request.params.id);
+    response.render('admin/edit.html', {product});
 });
-exports.post_products_edit = ((request, response)=> {
-    models.Products.update({
+exports.post_products_edit = (async (request, response)=> {
+    await models.Products.update({
         name : request.body.name,
         price : request.body.price,
         description : request.body.description
     }, {
         where : { id : request.params.id }
-    })
-    .then(()=> {
-        response.redirect('/admin/products/detail/'+request.params.id);
     });
+    response.redirect('/admin/products/detail/'+request.params.id);
 });
 
-exports.get_products_delete = ((request, response)=> {
-    models.Products.destroy({
+exports.get_products_delete = (async (request, response)=> {
+    await models.Products.destroy({
         where : { id : request.params.id }
-    })
-    .then(()=> {
-        response.redirect('/admin/products');
     });
+    response.redirect('/admin/products');
+    
 });
