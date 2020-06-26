98ac60ddb7b0380dd0f1adea747502661927d852
parkhyeongkyu <phg0644@gmail.com>
2020-06-26 15:11:11 +0900

    sequelize - DB insert  구현

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,4 +1,18 @@
+const models = require('../../models');
 
 exports.get_products = ((request, response)=> {
     response.render('admin/products.html')
+});
+
+
+exports.get_products_write = ((request, response)=> {
+    response.render('admin/write.html')
+});
+
+exports.post_products_write = ((request, response)=> {
+    // response.send(request.body);
+    models.Products.create(request.body)
+    .then(()=>{
+        response.redirect('/admin/products');
+    });
 });
--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -2,7 +2,13 @@ const {Router} = require('express');
 const router = Router();
 const ctrl = require('./admin.ctrl.js')
 
-router.use('/products', ctrl.get_products);
+router.get('/', (request, response)=>{
+    response.send('admin app');
+});
 
+router.get('/products', (ctrl.get_products));
+
+router.get('/products/write', (ctrl.get_products_write));
+router.post('/products/write', (ctrl.post_products_write));
 
 module.exports = router;
